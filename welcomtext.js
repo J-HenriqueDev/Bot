5e7871a50887d50a81ae1fe00a94ef5b0c3ca194
GugaVL <augusto015123@gmail.com>
2021-04-28 22:37:29 -0300

    First commit

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `Oiih @${number}. Seja bem_vindo(a) ao grupo ${groupname} leia as regras.`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. Já vai tarde deve uma mamada para os adm 👋`
+}
+exports.bye = bye
